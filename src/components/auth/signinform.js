99be2ccc664d88a69b83362b9b30125bc92a2104
c-masque <maudlin.oxalis@gmail.com>
2021-06-07 14:21:05 -0600

    Added submit/login button to sign in form

--- src/components/auth/signinform.js
+++ src/components/auth/signinform.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput } from '../formfields';
+import { FormInput, FormButton } from '../formfields';
 
 class SignInForm extends Component {
     render() {
-        const { className } = this.props;
+        const { className, handleSubmit } = this.props;
 
         return (
-            <form className={`${className} sign-in-form`}>
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className='sign-in-form__email'
                 name='Email'
                 type='Email'
@@ -23,6 +23,14 @@ class SignInForm extends Component {
                 title='Password'
                 placeholder='Password'
                 component={FormInput}/>
+
+                <Field className='sign-in-form__login'
+                name='Login'
+                onClick={() => console.log('tryna submit')}
+                type='submit'
+                title='Login'
+                placeholder='Login'
+                component={FormButton}/>
 
             </form>
         )
