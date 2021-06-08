1abaadb503e5ea5fa310e8bec94de44fe0dd5d30
c-masque <maudlin.oxalis@gmail.com>
2021-06-08 10:22:45 -0600

    Built detail components.

--- src/components/auth/signinform.js
+++ src/components/auth/signinform.js
@@ -3,10 +3,25 @@ import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formfields';
+import Details from '../details'
+
+import history from '../../history'
 
 class SignInForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+            {
+                _id: 0,
+                title: 'Not registered? Create account here',
+                onClick: () => history.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot email/password?',
+                onClick: () => console.log('forgot credentials')
+            }
+    ]
 
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
@@ -32,6 +47,8 @@ class SignInForm extends Component {
                 title='Login'
                 placeholder='Login'
                 component={FormButton}/>
+
+                <Details className='sign-in-form__details' title='QuickLinks' links={links} />
 
             </form>
         )
