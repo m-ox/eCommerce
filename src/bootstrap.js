5f8b24537f260717fab1ff402c9f42c8ed283d0d
c-masque <maudlin.oxalis@gmail.com>
2021-06-10 11:44:47 -0600

    built base for shop component, built shop route

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -11,6 +11,7 @@ import Layout from "./components/layout";
 import SignIn from './components/auth/signin'
 import SignUp from './components/auth/signup'
 import Account from './components/account/account'
+import Shop from "./components/shop/shop";
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)(createStore)));
 
@@ -26,6 +27,7 @@ function main() {
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
             <Route path='/account' component={Account} />
+            <Route path='/shop' component={Shop} />
           </Switch>
         </Layout>
       </Router>
---
+++ src/components/shop/shop.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Login',
+                path: '/signin'
+            },
+            {
+                path: '/'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks)
+        // fetch shop products action creator
+            // set the header links
+
+        // fetch navbar links
+            // set the navbar links
+            // filter products with links
+    }
+
+    render() {
+        return (
+            <div className='shop'>
+                {/* shop search bar component */}
+                {/* product components */}
+                {/* cart button */}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { state }
+}
+
+Shop = connect(mapStateToProps, actions) (Shop);
+
+export default Shop
