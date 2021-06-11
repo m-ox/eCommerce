cae4c1744fe02bcf0856629c1c151de5beb7f533
c-masque <maudlin.oxalis@gmail.com>
2021-06-11 14:37:15 -0600

    built footer component for shop cart

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -12,15 +12,35 @@ function CartButton({className, icon}) {
 
 function CartContent ({ className, products }) {
     let count = products.length
+    let productsJSX = products.map(product => <h1 key={product}> {product} </h1>)
+
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
                 Cart ({count})
             </div>
             <div className='cart-content__products'>
-
+                {productsJSX}
             </div>
             <div className='cart-content__footer'>
+                <CartFooter className='cart-content__footer' products={products} />
+            </div>
+        </div>
+    )
+}
+
+function CartFooter ({className, products}) {
+    const price = 7.96;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className='cart-footer__checkout'>
+                Checkout
+            </a>
+            <div className='cart-footer__subtotal'>
+                Subtotal
+            </div>
+            <div classname='cart-footer__price'>
+                ${price}
             </div>
         </div>
     )
