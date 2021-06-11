c2886d8b5a4bd07c724989724d2cb18ac8e20619
c-masque <maudlin.oxalis@gmail.com>
2021-06-11 11:02:22 -0600

    made search bar look better with react-icons and added grid styles... I would rather a container query here but I'm following the video

--- src/components/shop/shopSearchbar.js
+++ src/components/shop/shopSearchbar.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field } from 'redux-form';
+import { FaSearch } from 'react-icons/fa'
 
 function FormSearchBar(props) {
     return (
-        <input className={`${props.className} form-search-bar search-bar-grid__input`} {...props.input} type='text' placeholder={props.placeholder}/>
+        <div className='search-bar-grid'>
+            <FaSearch className="fas fa-search search-bar-grid__icon" />
+            <input className={`${props.className} form-search-bar search-bar-grid__input`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+        </div>
     )
 }
 
+
 class ShopSearchBar extends Component {
     render() {
-        const { className, handleSubmit } = this.props
+        const { className, handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
-                <Field name='query' className='shop-search-bar__form-search-bar' placeholder='🔍' component={FormSearchBar}/>
+                <Field name='query' className='shop-search-bar__form-search-bar' placeholder='Search' component={FormSearchBar} />
             </form>
         )
-    }
+    }    
 }
 
 ShopSearchBar = reduxForm({
     form: 'ShopSearchBar'
-})(ShopSearchBar)
+})(ShopSearchBar);
 
 export default ShopSearchBar;
